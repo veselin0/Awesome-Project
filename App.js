cb34f511f252e407747c951846f8c9337e09b4ed
Veselin <vesselin60@gmail.com>
2020-10-01 18:26:54 +0300

    Shopping list initial commit

--- App.js
+++ App.js
@@ -1,24 +1,103 @@
-import React, {useState} from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import React, {useState, useRef, useEffect} from 'react';
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const ITEM_HEIGHT = 44;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  list: {
+    flex: 1,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    lineHeight: 18,
+    height: ITEM_HEIGHT,
+    borderColor: 'red',
+    borderWidth: 1,
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderColor: 'black',
+    height: ITEM_HEIGHT,
+  },
+  button: {
+    height: ITEM_HEIGHT,
+  },
+});
+
+const FlatListBasics = () => {
+  const {text, setText, listRef, data, setData} = useListState();
 
   return (
     <View style={styles.container}>
-      <Text>You clicked {count} times</Text>
-      <Button onPress={() => setCount(count + 1)} title="Click me" />
+      <TextInput
+        style={styles.input}
+        value={text}
+        onChangeText={setText}
+        placeholder="Type here!"
+      />
+      <FlatList
+        style={styles.list}
+        ref={listRef}
+        data={data}
+        renderItem={({item, index}) => (
+          <Text style={styles.item}>
+            {index + 1}: {item.key}
+          </Text>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+      <Button
+        style={styles.button}
+        title="Add Item"
+        onPress={() => {
+          setData([...data, {key: text}]);
+          listRef.current.scrollToEnd();
+        }}
+      />
     </View>
   );
 };
 
-//React Native Styles:
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+const useListState = () => {
+  const [data, setData] = useState([]);
+  const [text, setText] = useState('');
+  const listRef = useRef();
 
-export default App;
+  /*
+    Use AsyncStorage setItem and getItem to read and write local changes
+    to phone memory in the effect hook bellow
+
+    const storedItems = await AsyncStorage ...
+    console.log({ storedItems });
+    setData(storedItems || []);
+   */
+  AsyncStorage.getItem('LIST_DATA');
+
+  useEffect(() => {
+    const readStoredItems = async () => {};
+    readStoredItems();
+  }, []);
+
+  return {
+    data,
+    setData,
+    text,
+    setText,
+    listRef,
+  };
+};
+
+export default FlatListBasics;
